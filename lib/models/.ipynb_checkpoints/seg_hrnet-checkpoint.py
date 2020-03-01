95daaaadb6a0f8651e211e3b8b7936def49f154b
Mendrika Ramarlina <m.ramarlina@gmail.com>
2020-03-01 07:48:19 +0000

    adding some consistency in reading the params

--- lib/models/.ipynb_checkpoints/seg_hrnet-checkpoint.py
+++ lib/models/.ipynb_checkpoints/seg_hrnet-checkpoint.py
@@ -279,7 +279,7 @@ class HighResolutionNet(nn.Module):
         stage1_out_channel = block.expansion*num_channels
 
         self.stage2_cfg = config.MODEL.EXTRA.STAGE2
-        num_channels = self.stage2_cf.NUM_CHANNELS
+        num_channels = self.stage2_cfg.NUM_CHANNELS
         block = blocks_dict[self.stage2_cf.BLOCK]
         num_channels = [
             num_channels[i] * block.expansion for i in range(len(num_channels))]
