ba78e089dcc15a8b8356e4ca79cc4408f254fd4d
Mendrika Ramarlina <m.ramarlina@gmail.com>
2020-03-01 13:10:27 +0000

    fixing missing parameter labels error

--- lib/models/.ipynb_checkpoints/seg_hrnet-checkpoint.py
+++ lib/models/.ipynb_checkpoints/seg_hrnet-checkpoint.py
@@ -21,7 +21,7 @@ import torch.nn.functional as F
 
 from inplace_abn import InPlaceABNSync
 
-BatchNorm2d = functools.partial(InPlaceABNSync, activation='none')
+BatchNorm2d = functools.partial(InPlaceABNSync, activation='identity')
 BN_MOMENTUM = 0.01
 logger = logging.getLogger(__name__)
 
